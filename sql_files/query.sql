20d95d510bd08f6dd904510f8b641ac46a803f1a
clarari137 <142682830+clarari137@users.noreply.github.com>
2024-01-15 15:53:57 -0600

    update query.sql
    select subset of columns to work with

---
+++ sql_files/query.sql
@@ -0,0 +1,5 @@
+SELECT OBJECTID, GEOID20, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE, CBSA_Name, CBSA_Pop, CBSA_Emp, 
+	CBSA_Wrk, Workers, Ac_Total, Pct_AO0, Pct_AO1, 
+	Ac_Water, Ac_Land, TotPop, CountHU, HH, TotEmp, 
+	E5_Ret, E5_Off, E5_Ind, D1A, D1B, D1C, NatWalkInd
+ FROM walkability;
